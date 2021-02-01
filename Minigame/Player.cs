f4309bc2a90ed263f9b44802f32cce0d50c49900
RoyalGuilds <Jared.trenholm12@gmail.com>
2021-02-01 14:53:31 -0400

    Initial commit

---
+++ Minigame/Player.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minigame
+{
+    class Player
+    {
+        public int playerX;
+        public int playerY;
+        public int MinPos = 3;
+        public int MaxPos = 50;
+        public int MaxPosX = 99;
+        public bool FiftyFifty = false;
+        public bool TenFive = false;
+
+
+        public Player()
+        {
+            playerX = 3;
+            playerY = 3;
+        }
+
+        public void DrawPlayer()
+        {
+            Console.SetCursorPosition(playerX, playerY);
+            Console.Write("@");
+        }
+        public void MovePlayer()
+        {
+            string input = Console.ReadKey(true).Key.ToString(); ;
+            if (input == ConsoleKey.W.ToString())
+            {
+                if(playerY > MinPos)
+                {
+                    playerY = playerY - 1;
+                    
+                }
+            } else if (input == ConsoleKey.A.ToString())
+            {
+                if (playerX > MinPos)
+                {
+                    playerX = playerX - 1;
+                    
+                }
+            }
+            else if (input == ConsoleKey.S.ToString())
+            {
+                if (playerY < MaxPos)
+                {
+                    playerY = playerY + 1;
+                    
+                }
+            }
+            else if (input == ConsoleKey.D.ToString())
+            {
+                if (playerX < MaxPosX)
+                {
+                    playerX = playerX + 1;
+                    
+                }
+
+            }
+        }
+
+        public void DetectEvents()
+        {
+            if (playerX == 50)
+            {
+                if (playerY == 50)
+                {
+                    if (FiftyFifty == false)
+                    {
+                        Console.WriteLine("You discovered a town here!");
+                        Console.WriteLine("Press enter to continue.");
+                        Console.ReadKey(true);
+                        FiftyFifty = true;
+                    }
+                }
+            } else if (playerX == 10)
+            {
+                if (playerY == 5)
+                {
+                    if (TenFive == false)
+                    {
+                        Console.WriteLine("You discovered a dungeon here!");
+                        Console.WriteLine("Press enter to continue.");
+                        Console.ReadKey(true);
+                        TenFive = true;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Minigame/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minigame
+{
+    class Program
+    {
+        static Player player = new Player();
+        static void Main(string[] args)
+        {
+            int Loop = 0;
+            while (Loop < 1)
+            {
+                Console.Clear();
+                player.DetectEvents();
+                Console.Clear();
+                DrawMap();
+                player.DrawPlayer();
+                player.MovePlayer();
+
+            }
+        }
+        static void DrawMap()
+        {
+            for (int x = 0; x < 1;)
+            {
+                Console.WriteLine("Move: WASD                    Player Position: " + player.playerX + ", " + player.playerY);
+                Console.Write("┌");
+                for (int y = 0; y < 99; y++)
+                {
+                    Console.Write("─");
+                }
+                Console.WriteLine("┐");
+                for (int y = 2; y < 51; y++)
+                {
+                    Console.WriteLine("│");
+                    Console.SetCursorPosition(100, y);
+                    Console.WriteLine("│");
+                }
+                Console.Write("└");
+                for (int y = 0; y < 99; y++)
+                {
+                    Console.Write("─");
+                }
+                Console.WriteLine("┘");
+                x = 1;
+            }
+            if(player.FiftyFifty == true)
+            {
+                Console.SetCursorPosition(50,50);
+                Console.Write("T");
+            }
+            if (player.TenFive == true)
+            {
+                Console.SetCursorPosition(10, 5);
+                Console.Write("D");
+            }
+        }
+    }
+}
